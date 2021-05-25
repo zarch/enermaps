965eace55452996394b636ccd579f43ec45bd68e
Mélanie Huck <melanie.huck@protonmail.ch>
2021-05-25 15:10:10 +0200

    Adding layers description

---
+++ api/app/layers.py
@@ -0,0 +1,10 @@
+"""
+This file contains all the layers that are listed in the interface and
+potentially queryable.
+"""
+
+OVERLAY_LAYERS = {
+    1: {"display name": "test layer 01", "id": 1, "type": "raster"},
+    2: {"display name": "test layer 02", "id": 2, "type": "geojson"},
+    3: {"display name": "test layer 03", "id": 3, "type": "vector"},
+}
