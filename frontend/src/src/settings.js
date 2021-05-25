7e7b7360b2a883b78fb04994969f36bb12b22627
Mélanie Huck <melanie.huck@protonmail.ch>
2021-05-25 15:11:08 +0200

    Adding frontend layer description

--- frontend/src/src/settings.js
+++ frontend/src/src/settings.js
@@ -7,3 +7,33 @@ export const BASE_LAYER_PARAMS= {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">'+
     'OpenStreetMap</a> &copy; <a href="https://cartodb.com/attributions">CartoDB</a>',
 };
+
+export const SELECTION_LAYERS = {
+  "NUTS 0" : 0,
+  "NUTS 1" : 1,
+  "NUTS 2" : 2,
+  "NUTS 3" : 3,
+  "LAU" : 4
+};
+
+export const OVERLAY_LAYERS =
+{
+  "test_layer_01 " :
+    {
+      "display name" : "test layer 01",
+      "id" : 1,
+      "type" : "raster"
+    },
+  "test_layer_02" :
+  {
+    "display name" : "test layer 02",
+    "id" : 2,
+    "type" : "geojson"
+  },
+  "test_layer_03" :
+  {
+    "display name" : "test layer 03",
+    "id" : 3,
+    "type" : "vector"
+  },
+};
