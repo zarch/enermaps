172236271478fe71085c5d3adc31ec80d13624fa
Mélanie Huck <melanie.huck@protonmail.ch>
2021-05-26 17:05:34 +0200

    Adding layers description file

--- api/app/layers.py
+++ api/app/layers.py
@@ -4,7 +4,13 @@ potentially queryable.
 """
 
 OVERLAY_LAYERS = {
-    1: {"display name": "test layer 01", "id": 1, "type": "raster"},
-    2: {"display name": "test layer 02", "id": 2, "type": "geojson"},
-    3: {"display name": "test layer 03", "id": 3, "type": "vector"},
+    "1": {"display name": "test layer 01", "id": 1, "type": "raster"},
+    "2": {"display name": "test layer 02", "id": 2, "type": "geojson"},
+    "3": {"display name": "test layer 03", "id": 3, "type": "vector"},
+    "nuts0.zip": {"display name": "NUTS 0", "id": 4, "type": "geojson"},
+    "nuts1.zip": {"display name": "NUTS 1", "id": 5, "type": "geojson"},
+    "nuts2.zip": {"display name": "NUTS 2", "id": 6, "type": "geojson"},
+    "nuts3.zip": {"display name": "NUTS 3", "id": 7, "type": "geojson"},
+    "lau.zip": {"display name": "LAU", "id": 8, "type": "geojson"},
+    "gfa_tot_curr_density.tiff": {"display name": "density", "id": 9, "type": "raster"},
 }
